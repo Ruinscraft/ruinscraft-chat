6e3c7fecf5c90b6481cb20b8c99e6a1c15fda3dd
Dexton Anderson <contact@dextonanderson.com>
2018-11-24 15:26:59 -0600

    ConsoleChatLogger can now handle PMs

--- src/main/java/com/ruinscraft/chat/logging/ConsoleChatLogger.java
+++ src/main/java/com/ruinscraft/chat/logging/ConsoleChatLogger.java
@@ -1,12 +1,18 @@
 package com.ruinscraft.chat.logging;
 
 import com.ruinscraft.chat.message.ChatMessage;
+import com.ruinscraft.chat.message.PrivateChatMessage;
 
 public class ConsoleChatLogger implements ChatLogger {
 
 	@Override
 	public void log(ChatMessage message) {
-		System.out.println(message.getSender() + ": " + message.getPayload());
+		if (message instanceof PrivateChatMessage) {
+			PrivateChatMessage pm = (PrivateChatMessage) message;
+			System.out.println("[" + pm.getSender() + " -> " + pm.getRecipient() + "] " + pm.getPayload());
+		} else {
+			System.out.println("[" + message.getSender() + "] " + message.getPayload());
+		}
 	}
 	
 }
