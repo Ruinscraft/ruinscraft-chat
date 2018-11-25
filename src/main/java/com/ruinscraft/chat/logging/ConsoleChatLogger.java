fec007101b99ffd27f6eab5ffa836b264c82ddbb
Dexton Anderson <contact@dextonanderson.com>
2018-11-25 14:38:48 -0600

    modify console chat logger

--- src/main/java/com/ruinscraft/chat/logging/ConsoleChatLogger.java
+++ src/main/java/com/ruinscraft/chat/logging/ConsoleChatLogger.java
@@ -1,17 +1,27 @@
 package com.ruinscraft.chat.logging;
 
+import com.ruinscraft.chat.ChatPlugin;
 import com.ruinscraft.chat.message.ChatMessage;
 import com.ruinscraft.chat.message.PrivateChatMessage;
 
 public class ConsoleChatLogger implements ChatLogger {
 
+	private static final String PREFIX = "[CHAT] ";
+	
 	@Override
 	public void log(ChatMessage message) {
 		if (message instanceof PrivateChatMessage) {
 			PrivateChatMessage pm = (PrivateChatMessage) message;
-			System.out.println("[" + pm.getSender() + " -> " + pm.getRecipient() + "] " + pm.getPayload());
+			System.out.println(PREFIX + "[" + pm.getSender() + " -> " + pm.getRecipient() + "] " + pm.getPayload());
 		} else {
-			System.out.println("[" + message.getSender() + "] " + "[" + message.getServerSentFrom() + "] " + message.getPayload());
+			// Don't log to console if not global and not the same server
+			if (!message.getIntendedChannelName().equals("global")) {
+				if (!message.getServerSentFrom().equals(ChatPlugin.getInstance().getServerName())) {
+					return;
+				}
+			}
+			
+			System.out.println(PREFIX + "[" + message.getSender() + "] " + "[" + message.getServerSentFrom() + "] " + message.getPayload());
 		}
 	}
 	
